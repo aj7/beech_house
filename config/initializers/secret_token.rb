d6c37da853c7c6b94a9e4a5805882c3b9952ae73
aj7 <ackbar@ackadij.com>
2010-11-04 22:17:02 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BeechHouse::Application.config.secret_token = 'a84a2b759cd6761afb93bae3d21fd83af8b15a358e0722650545a006d04d568d1ef4c7f28bd731f31c0355f9d46a82fc8c856172af30040fd8691b74372ed4c0'
